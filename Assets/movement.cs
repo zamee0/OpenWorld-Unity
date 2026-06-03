d435a3f56fd7c06a03b037ec2079d48a80edd151
zamee0 <zamee5721@gmail.com>
2026-06-03 20:58:03 +0600

    added animation

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -6,17 +6,31 @@ public class movement : MonoBehaviour
 
     private Rigidbody2D rb;
     private Vector2 v;
+    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         v.x = Input.GetAxisRaw("Horizontal");
         v.y = Input.GetAxisRaw("Vertical");
-        v.Normalize() ;
+
+        v.Normalize();
+
+        animator.SetBool("isWalking", v != Vector2.zero);
+
+        animator.SetFloat("InputX", v.x);
+        animator.SetFloat("InputY", v.y);
+
+        if (v != Vector2.zero)
+        {
+            animator.SetFloat("LastInputX", v.x);
+            animator.SetFloat("LastInputY", v.y);
+        }
     }
 
     void FixedUpdate()
